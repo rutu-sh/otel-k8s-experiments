8932b029348c42ce7541fe100d60e0803bb1f039
rutu-sh <ruturajs10@hotmail.com>
2023-10-17 09:11:07 +0530

    feat: add logging instrumentation

--- common-applications/auto-instrumented/python/simple-fastapi-app/src/main.py
+++ common-applications/auto-instrumented/python/simple-fastapi-app/src/main.py
@@ -1,12 +1,20 @@
+import os
 import logging
 
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.responses import JSONResponse
 
-
-logging.basicConfig(level=logging.DEBUG)
+os.environ["LOG_LEVEL"] = "DEBUG"
 logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+items = []
 
 app = FastAPI()
 
@@ -14,19 +22,59 @@ app = FastAPI()
 @app.get("/")
 def health_check():
     logger.info("Health check called")
-    return {"status": "ok"}
+    return JSONResponse(status_code=200, content={"message": "Application is up and running"})
 
 
-@app.get("/hello/{name}")
-def hello(name: str):
-    logger.info("Hello called")
-    return {"message": f"Hello {name}!"}
+@app.get("/rest/v1/items")
+def get_items():
+    logger.info("Get items called")
+    return JSONResponse(status_code=200, content={"items": {item_id: item.dict() for item_id, item in enumerate(list(filter(None, items)))}})
 
 
-@app.get("/error")
-def error():
-    logger.error("Error called")
-    return JSONResponse(status_code=500, content={"message": "Error"})
+@app.get("/rest/v1/item/{id}", response_model=Item)
+def get_item(id: int):
+    logger.info("Get items called")
+    try:
+        if items[id] is None:
+            logger.error(f"Item not found for id: {id}")
+            return JSONResponse(status_code=404, content={"message": "Item not found"})
+        logger.debug(f"Item found for id: {id}")
+        return items[id]
+    except IndexError:
+        logger.error(f"Item not found for id: {id}")
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
+
+
+@app.post("/rest/v1/item")
+def add_item(item: Item):
+    logger.info("Add item called")
+    items.append(item)
+    return JSONResponse(status_code=201, content={"message": "Item added successfully", "item_id": len(items) - 1})
+
+
+@app.delete("/rest/v1/item/{id}")
+def delete_item(id: int):
+    logger.info("Delete item called")
+    try:
+        logger.debug(f"Deleting item for id: {id}")
+        items[id] = None
+        logger.debug(f"Item deleted for id: {id}")
+        return JSONResponse(status_code=200, content={"message": "Item deleted successfully"})
+    except IndexError:
+        logger.error(f"Item not found for id: {id}")
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
+
+
+@app.put("/rest/v1/item/{id}")
+def update_item(id: int, item: Item):
+    logger.info("Update item called")
+    try:
+        logger.debug(f"Updating item for id: {id}")
+        items[id] = item
+        logger.debug(f"Item updated for id: {id}")
+        return JSONResponse(status_code=200, content={"message": "Item updated successfully"})
+    except IndexError:
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
 
 
 def main():
